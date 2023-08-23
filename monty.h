4b977410088f565b402cf424abe38013046792b3
Arnold Henry <devharnold@gmail.com>
2023-08-23 10:42:28 +0300

    Header-file

--- monty.h
+++ monty.h
@@ -47,6 +47,8 @@ void push_queue(stack_t **stack, unsigned int param);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t  **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void divide(stack_t **stack, unsigned int line_number);
