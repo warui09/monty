17ec0ef17ef9a7b4dcb06afa26b45e742d2be488
Arnold Henry <devharnold@gmail.com>
2023-08-23 22:45:05 +0300

    pint-c

--- pint.c
+++ pint.c
@@ -1,5 +1,14 @@
 #include "monty.h"
 
+/*
+ * pint - Prints the value at the top of the stack 
+ *        Followed by a new line
+ * @stack: Head of stack 
+ * @line_number: Line of error occurence
+ * 
+ * Return: Nothing
+*/
+
 void pint(stack_t **stack, unsigned int line_number) {
     if (*stack == NULL) {
         fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
