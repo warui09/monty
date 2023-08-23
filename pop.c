bbafb3ada4d115108197c90543197d9962c190d9
Arnold Henry <devharnold@gmail.com>
2023-08-23 22:55:34 +0300

    poppy

---
+++ pop.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/*
+ * pop - Removes the top element of the stack
+ * @stack: Head ofthe stack
+ * @line_number: Line of error occurence
+ * 
+ * Return: Nothing
+*/
+void pop(stack_t **stack, unsigned int line_number){
+    if (*stack == NULL){
+        fprintf(stderr, "L%d: can't pop this empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *temp = *stack;
+    *stack = (*stack) -> next;
+    free(temp);
+}
