057b43d051e067dd37e6d00032a97226fc8bd140
Arnold Henry <devharnold@gmail.com>
2023-08-23 15:10:04 +0300

    palls

---
+++ op_pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/*
+ * pall - Prints all the vaalues of the stack
+ *        starting from the top
+ * @stack - Head of the stack
+ * @line_number - Line on erorr occurence
+ * 
+ * Return - Nothing
+*/
+void pall(stack_t **stack, unsigned int line_number){
+    stack_t *current = NULL;
+    (void) line_number;
+
+    if(*stack){
+        current = *stack;
+        while (current != NULL){
+            printf("%d\n", current -> n);
+            current = current -> next;
+        }
+    }
+}
