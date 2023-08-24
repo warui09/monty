50dd6e278468635207c6a2b1215e7a8e07237ab9
Arnold Henry <devharnold@gmail.com>
2023-08-24 18:06:55 +0300

    poppy

--- pop.c
+++ pop.c
@@ -8,12 +8,14 @@
  * Return: Nothing
 */
 void pop(stack_t **stack, unsigned int line_number){
+    stack_t *temp;
+
     if (*stack == NULL){
         fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    stack_t *temp = *stack;
+    temp = *stack;
     *stack = (*stack) -> next;
     free(temp);
 }
