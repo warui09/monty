bf539a0294dfe919dd098d4110ea88919b2195f7
Arnold Henry <devharnold@gmail.com>
2023-08-24 11:52:28 +0300

    adds-c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/*
+ * add - Adds the top two elements of the stack
+ * @stack: The head of the stack
+ * @line_number - Line of error occurence
+ * 
+ * Return - Nothing
+*/
+void add(stack_t **stack, unsigned int line_number){
+    
+}
