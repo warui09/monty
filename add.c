e929b5329c330dbae20357b457a5e8691c14d717
Arnold Henry <devharnold@gmail.com>
2023-08-24 12:15:47 +0300

    we-add

--- add.c
+++ add.c
@@ -8,5 +8,18 @@
  * Return - Nothing
 */
 void add(stack_t **stack, unsigned int line_number){
-    
+    	unsigned int length = 0, temp = 0;
+
+	length = count_stack(*stack);
+
+	if (length < 2)
+		fprintf(stderr, "L%d: Failed to add stack!\n", line_number);
+        exit(EXIT_FAILURE);
+
+	if (*stack)
+	{
+		temp = (*stack) -> n;
+		(*stack) -> n = (*stack) -> next -> n;
+		(*stack) -> next -> n = temp;
+	}
 }
