3b5033132d7b338a14098df42763735d13ba6124
Arnold Henry <devharnold@gmail.com>
2023-08-25 09:50:20 +0300

    changes

--- pop.c
+++ pop.c
@@ -21,8 +21,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n",
-				line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
