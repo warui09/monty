b53925f89f2a1215ce219d56f56db5258967fc38
Arnold Henry <devharnold@gmail.com>
2023-08-25 12:25:34 +0300

    last-changes

--- pstr.c
+++ pstr.c
@@ -14,7 +14,7 @@ void pstr(stack_t **stack, unsigned int line_number){
     (void) line_number;
 
     while (curr != NULL){
-        if (curr -> n == 0 || curr -> n < 0 || curr -> n > 127)
+        if (curr -> n <= 0 || curr -> n > 127)
         break;
 
         printf("%c", curr -> n);
